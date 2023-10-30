34bd337ab6a08a96a505158ccede3d8e25a80b0b
Naïm Favier <n@monade.li>
2023-10-30 23:13:19 +0100

    minor improvements

--- flake.nix
+++ flake.nix
@@ -14,18 +14,30 @@
         (agda.withPackages (p: with p; [ cubical _1lab ]))
       ];
       LC_ALL = "C.UTF-8";
-      buildPhase =  ''
+      extraHead = ''
+        <meta name="author" content="Naïm Favier">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+        <link rel="icon" type="image/png" href="//monade.li/favicon.png">
+      '';
+      buildPhase = ''
         shopt -s extglob
         for f in src/*.agda src/*.lagda*; do
-          agda --html --html-dir="$out" --html-highlight=all --css=style.css --highlight-occurrences "$f"
           mod=''${f%%.@(agda|lagda*)} mod=''${mod##*/}
+          if grep -qE 'import *(1Lab|Cat)\.' "$f"; then
+            echo "import $mod" >> Everything-1Lab.agda
+          else
+            echo "import $mod" >> Everything.agda
+          fi
           cat >> mods << EOF
-          <a class="Keyword">import</a> <a href="$mod.html" class="Module">$mod</a>
+        <a class="Keyword">import</a> <a href="$mod.html" class="Module">$mod</a>
         EOF
         done
+        agda -i . --html --html-dir="$out" --html-highlight=all --css=style.css --highlight-occurrences Everything.agda
+        agda -i . --html --html-dir="$out" --html-highlight=all --css=style.css --highlight-occurrences Everything-1Lab.agda
+        substituteInPlace "$out"/highlight-hover.js --replace getSelectorAll querySelectorAll
+        substituteInPlace "$out"/*.html --replace '</head>' "$extraHead</head>"
         cp ${self}/{index.html,style.css} "$out"/
-        substituteInPlace "$out"/index.html --replace '@modules@' "$(< mods)"
-        sed -i 's/getSelectorAll/querySelectorAll/g' "$out"/highlight-hover.js
+        substituteInPlace "$out"/index.html --subst-var-by modules "$(< mods)"
       '';
     };
   };
