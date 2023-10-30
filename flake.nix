b50b4bdc45ea9b46ba045301918ec0d049dada5f
Naïm Favier <n@monade.li>
2023-10-30 18:35:19 +0100

    add html generation

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/haskell-updates";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      name = "cubical-experiments";
+      src = self;
+      nativeBuildInputs = with pkgs; [
+        (agda.withPackages (p: with p; [ cubical _1lab ]))
+      ];
+      LC_ALL = "C.UTF-8";
+      buildPhase =  ''
+        shopt -s extglob
+        for f in *.agda *.lagda*; do
+          agda --html --html-dir="$out" --html-highlight=all --css=style.css --highlight-occurrences "$f"
+          mod=''${f%%.@(agda|lagda*)}
+          cat >> mods << EOF
+          <a class="Keyword">import</a> <a href="$mod.html" class="Module">$mod</a>
+        EOF
+        done
+        cp ${self}/{index.html,style.css} "$out"/
+        substituteInPlace "$out"/index.html --replace '@modules@' "$(< mods)"
+        sed -i 's/getSelectorAll/querySelectorAll/g' "$out"/highlight-hover.js
+      '';
+    };
+  };
+}
